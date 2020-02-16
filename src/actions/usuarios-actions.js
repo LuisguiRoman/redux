fd4b4fbbba0b542dc7d65b5a1d78ec85d63eb0ce
Luisgui <luisguirohi@gmail.com>
2020-02-16 17:01:03 -0500

    Creacion de types usuarios para prevenir errores

--- src/actions/usuarios-actions.js
+++ src/actions/usuarios-actions.js
@@ -1,4 +1,5 @@
-import axios from 'axios'
+import axios from 'axios';
+import { TRAER_TODOS } from '../types/usuarios-types';
 
 //una funcion que retorna otra funcion
 export  const traerTodos = () => async (dispatch) => {
@@ -6,7 +7,7 @@ export  const traerTodos = () => async (dispatch) => {
     const response = await axios.get('https://jsonplaceholder.typicode.com/users');
 
     dispatch({
-        type: 'traer_usuarios',
+        type: TRAER_TODOS,
         payload: response.data
     });
 }
