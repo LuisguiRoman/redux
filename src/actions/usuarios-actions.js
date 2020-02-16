94948b038ec63b2337cc30a74483e892d346c85e
Luisgui <luisguirohi@gmail.com>
2020-02-16 17:58:43 -0500

    creacion de types

--- src/actions/usuarios-actions.js
+++ src/actions/usuarios-actions.js
@@ -1,13 +1,20 @@
 import axios from 'axios';
-import { TRAER_TODOS } from '../types/usuarios-types';
+import { TRAER_TODOS, LOADING } from '../types/usuarios-types';
 
 //una funcion que retorna otra funcion
 export  const traerTodos = () => async (dispatch) => {
-    //hacer la peticion
-    const response = await axios.get('https://jsonplaceholder.typicode.com/users');
-
     dispatch({
-        type: TRAER_TODOS,
-        payload: response.data
+        type: LOADING
     });
+
+    try {
+        //hacer la peticion
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        dispatch({
+            type: TRAER_TODOS,
+            payload: response.data
+        });
+    } catch (error) {
+        console.log(error);
+    }
 }
--- src/reducers/usuarios-reducer.js
+++ src/reducers/usuarios-reducer.js
@@ -1,13 +1,20 @@
-import { TRAER_TODOS } from '../types/usuarios-types';
+import { TRAER_TODOS, LOADING } from '../types/usuarios-types';
 
 const INITIAL_STATE = {
-    usuarios: []
+    usuarios: [],
+    loading: false
 };
 
 export default (state = INITIAL_STATE, action) =>{
     switch(action.type){
         case TRAER_TODOS:
-            return { ...state, usuarios: action.payload }
+            return {
+                ...state, 
+                usuarios: action.payload,
+                loading: false
+            };
+        case LOADING:
+            return { ...state, loading: true };
         default: return state
     }
 };
