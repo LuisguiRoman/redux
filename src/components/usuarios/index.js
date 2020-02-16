87e627b4cdef5b6f6ac1da5d23e9658250ff7737
Luisgui <luisguirohi@gmail.com>
2020-02-16 18:39:44 -0500

    Separacion por componentes

--- src/components/usuarios/index.js
+++ src/components/usuarios/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import Table from './Tabla';
 import { Spinner } from '../general/Spinner';
 
 import * as usuariosActions from '../../actions/usuarios-actions';
@@ -10,37 +10,13 @@ class Usuarios extends Component{
     componentDidMount(){
         this.props.traerTodos();
     }
-
-    putContent = () => (
-        <div className="margen">
-            <table className="tabla">
-                <thead>
-                    <tr>
-                        <th>Nombre</th>
-                        <th>Correo</th>
-                        <th>Enlace</th>
-                    </tr>
-                </thead>
-
-                <tbody>
-                    {this.props.usuarios.map((usuario)=>(
-                        <tr key={usuario.id}>
-                            <td>{usuario.name}</td>
-                            <td>{usuario.email}</td>
-                            <td>{usuario.website}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    );
     
     render(){
         return(
             <div>
                 {this.props.loading ? 
                     <Spinner /> :
-                    this.putContent()
+                    <Table />
                 }
             </div>
         );
